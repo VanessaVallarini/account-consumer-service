7b0c8b135bb36ac1f3cfba519f719a63ff3d4f1f
Vanessa Vallarini <vanessavallarini@gmail.com>
2022-11-26 16:16:58 -0300

    refactor main

--- cmd/account-consumer-service/listner/listner.go
+++ cmd/account-consumer-service/listner/listner.go
@@ -4,14 +4,14 @@ import (
 	"account-consumer-service/internal/models"
 	"account-consumer-service/internal/pkg/kafka"
 	"account-consumer-service/internal/pkg/services"
+	"account-consumer-service/internal/pkg/utils"
 	"context"
-	"fmt"
 )
 
 func Start(ctx context.Context, cfg *models.KafkaConfig, asc *services.AccountService) {
 	kafkaClient, _ := kafka.NewKafkaClient(cfg)
 	err := kafka.NewConsumer(ctx, cfg, kafkaClient, asc)
 	if err != nil {
-		fmt.Println(err)
+		utils.Logger.Error("Error consumer msg: %v", err)
 	}
 }
--- cmd/account-consumer-service/main.go
+++ cmd/account-consumer-service/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	api "account-consumer-service/api/account"
+	"account-consumer-service/cmd/account-consumer-service/health"
 	"account-consumer-service/cmd/account-consumer-service/listner"
+	"account-consumer-service/cmd/account-consumer-service/server"
 	"account-consumer-service/internal/config"
+	"account-consumer-service/internal/models"
 	"account-consumer-service/internal/pkg/db"
 	"account-consumer-service/internal/pkg/kafka"
 	"account-consumer-service/internal/pkg/repository"
@@ -11,46 +14,56 @@ import (
 	"account-consumer-service/internal/pkg/utils"
 	"account-consumer-service/pkg"
 	"context"
+
+	"github.com/labstack/echo"
 )
 
 func main() {
+
 	ctx := context.Background()
 
 	config := config.NewConfig()
 
 	scylla, err := db.NewScylla(config.Database)
 	if err != nil {
-		utils.Logger.Warn("error during create scylla", err)
+		utils.Logger.Error("error during create scylla", err)
 	}
 	defer scylla.Close()
 
 	accountRepository := repository.NewAccountRepository(scylla)
 	accountServiceConsumer := services.NewAccountService(accountRepository)
 
+	kafkaClient, err := kafka.NewKafkaClient(config.Kafka)
+	if err != nil {
+		utils.Logger.Warn("error during create kafka client")
+	}
+
+	kafkaProducer, err := kafkaClient.NewProducer()
+	if err != nil {
+		utils.Logger.Warn("error during kafka producer")
+	}
+
+	accountServiceProducer := pkg.NewAccountServiceProducer(*kafkaProducer)
+
 	go func() {
-
-		kafkaClient, err := kafka.NewKafkaClient(config.Kafka)
-		if err != nil {
-			utils.Logger.Warn("error during kafka client")
-		}
-
-		kafkaProducer, err := kafkaClient.NewProducer()
-		if err != nil {
-			utils.Logger.Warn("error during kafka producer")
-		}
-
-		accountServiceProducer := pkg.NewAccountServiceProducer(*kafkaProducer)
-		accountApi := api.NewAccountApi(accountServiceProducer)
-		server := utils.NewServer()
-		accountApi.Register(server.Server)
-
-		listner.Start(ctx, config.Kafka, accountServiceConsumer)
-
-		server.Start(config)
+		setupHttpServer(accountServiceProducer, config)
 	}()
+
+	go listner.Start(ctx, config.Kafka, accountServiceConsumer)
 
 	utils.Logger.Info("start application")
 
-	utils.NewHealthServer()
+	health.NewHealthServer()
 
+}
+
+func setupHttpServer(asp *pkg.AccountServiceProducer, config *models.Config) *echo.Echo {
+
+	accountApi := api.NewAccountApi(asp)
+	s := server.NewServer()
+	accountApi.Register(s.Server)
+
+	s.Start(config)
+
+	return s.Server
 }
